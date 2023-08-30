85dd894c371d9e06b9bc6ca0e2b1bb78d67076df
sayan990 <maximilianocaceres1601@gmail.com>
2023-08-30 17:18:23 -0300

    modified:   src/components/AboutUs.js

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -8,11 +8,14 @@ export default function AboutUs() {
       <h1  className="text-white text-xl flex font-semibold text-center p-10">Contactanos </h1>
     <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
         <li className="relative rounded-md p-3 ">
-        <h3 className="text-sm font-medium leading-5">Mail:</h3>
+        <div className="flex flex-row">
+        <h3 className="text-sm font-medium ">Mail:</h3>
         <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-        <li className="relative rounded-md p-3 hover:bg-gray-100">maximilianocaceres1601@gmail.com</li>
+        <li className="relative rounded-md p-3 text-base">maximilianocaceres1601@gmail.com</li>
         </ul>
+        </div>
         </li>
+        
     </ul>
     </div>
   );
